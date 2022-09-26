c7f171c04d25473f5bedccd678c974a0b36824ae
Patricio <patgarcia@users.noreply.github.com>
2022-09-26 09:18:30 -0400

    Add test cases

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const isPalindrome = str => str.length < 2 ? true : str[0] !== str.slice(-1) ? false : isPalindrome(str.slice(1, -1));
+
+// TEST CASES
+console.log(isPalindrome("") === true);
+console.log(isPalindrome("a") === true);
+console.log(isPalindrome("aa") === true);
+console.log(isPalindrome("aba") === true);
+console.log(isPalindrome("abba") === true);
+console.log(isPalindrome("abccba") === true);
+
+console.log(isPalindrome("ab") === false);
+console.log(isPalindrome("abc") === false);
+console.log(isPalindrome("abca") === false);
+console.log(isPalindrome("abcdba") === false);
